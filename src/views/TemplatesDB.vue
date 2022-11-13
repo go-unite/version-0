771ed22f21de1551aca4348d090bdcfc3afc4eb3
ianialator <ian.harris.ish@gmail.com>
2022-11-12 17:55:50 -0700

    setting up hosting test
    Co-authored-by: Michael Harris <meikul.harris@gmail.com>

--- src/views/TemplatesDB.vue
+++ src/views/TemplatesDB.vue
@@ -1,7 +1,51 @@
 <template>
-  All the templates
+  <div v-for="template in templates" class="box">
+    <div class="card-content">
+      <div class="content">
+        {{ template.content }}
+      </div>
+    </div>  
+  </div>
 </template>
 
-<script>
+<script setup>
+  import { doc, getDoc } from '../firebase/firebase.js'
+  import { onMounted, ref } from 'vue'
+  import {  } from '../firebase/firebase.js'
 
+  const templates = collection(db, [
+ /*    {
+      id: 'id1',
+      content: 'Garbage Pickup',
+    },
+    {
+      id: 'id2',
+      content: 'Another thing',
+    } */
+  ])
+  
+  onMounted(async () => {
+   /*  const querySnapshot = await getDocs(collection(db, "Prototype"));
+    let fbTemplates = [];
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data())
+      const template = {
+        id: doc.id,
+        name: doc.data().conent,
+        steps: doc.data().conent
+      }
+      fbTemplates.push(template)
+    })
+    todo.value = fbTemplates */
+    const fbTemplates = []
+    QuerySnapshot.forEach((doc) => {
+      const template = {
+        id: doc.id,
+        name: doc.data().name,
+        steps: doc.data().steps
+      }
+      fbTemplates.push(template)
+    })
+    templates.value = fbTemplates
+  })
 </script>
